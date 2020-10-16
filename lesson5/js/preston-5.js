d8d8f324fcc49cc805420436ee422e4c48c51d4a
siminie.github.io <simonelj@byui.edu>
2020-10-16 19:38:58 +0200

    dsgsdg

---
+++ lesson5/js/preston-5.js
@@ -0,0 +1,20 @@
+
+
+const hambutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive')
+}, false);
+
+try {
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    };
+    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+} catch (e) {
+    alert('Error with code or your browser does not support Locale');
+}
